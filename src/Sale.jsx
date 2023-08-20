0bd809559c95fb80df80c6665acabff2473184f8
Ozod <bukhorizodao@gmail.com>
2023-08-21 00:22:16 +0500

    changed component visibility with useState

--- src/Sale.jsx
+++ src/Sale.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
 export default function Sale() {
-    const deleteComponent = () => {
-        return false;
-    }
-    return (
-        <div className="mainSale">
-            <div className="off-sale">
-                <img src="sun.png" alt="sun" className="sun" />
-                <p className="off-parag"><b>-40% OFF Sale</b> on Atlas tests with promo code &#039;atlas2023&#039;</p>
-                <button className="buy">Buy now</button>
+ const [showComponent, setShowComponent] = useState(true);
+
+  const deleteComponent = () => {
+    setShowComponent(!showComponent);
+    console.log("Component is deleted")
+  };
+  return (
+    <>
+     {showComponent && (
+    <div className="mainSale">
+              <div className="off-sale">
+              <img src="sun.png" alt="sun" className="sun" />
+              <p className="off-parag">
+                <b>-40% OFF Sale</b> on Atlas tests with promo code
+                &#039;atlas2023&#039;
+              </p>
+              <button className="buy">Buy now</button>
             </div>
             <div className="delete-container">
-                <img onClick={deleteComponent} src="delete-btn.png" alt="delete" className="delete"/>
+              <img
+                onClick={deleteComponent}
+                src="delete-btn.png"
+                alt="delete"
+                className="delete"
+              />
             </div>
-        </div>
-    );
+    </div>
+    )}
+    </>
+  );
 }
