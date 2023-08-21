9bbab79fd318012dc5b18e4bfb0cff6789c6f7b4
Ozod <bukhorizodao@gmail.com>
2023-08-21 12:59:09 +0500

    styled dna component

--- src/Dna.jsx
+++ src/Dna.jsx
@@ -1,7 +1,16 @@
 export default function Dna() {
     return (
         <div className="dna-container">
-            <p>🧬 DNA</p>
+            <p className="dna-card">&#129516; DNA <span className="grey">104</span></p>
+            <p className="dna-card">&#128214; Guides <span className="grey">33</span></p>
+            <p className="dna-card">&#127973; Health <span className="grey">166</span></p>
+            <p className="dna-card">&#127874; Lifestyle <span className="grey">33</span></p>
+            <p className="dna-card">&#129440; Microbiome <span className="grey">79</span></p>
+            <p className="dna-card">&#128240; News <span className="grey">5</span></p>
+            <p className="dna-card">&#129361; Nutrition <span className="grey">166</span></p>
+            <p className="dna-card">&#128300; Oncology <span className="grey">104</span></p>
+            <p className="dna-card">&#127835; Recipes <span className="grey">5</span></p>
+            <p className="dna-card">&#x1F496; Reviews <span className="grey">244</span></p>
         </div>
     );
 }
