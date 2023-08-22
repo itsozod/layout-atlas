96d241b3532231c15c4e155da162d5015535740f
Ozod <bukhorizodao@gmail.com>
2023-08-22 15:32:23 +0500

    refactored dna component

---
+++ src/Dna-data.jsx
@@ -0,0 +1,62 @@
+export const dnaData = [
+  {
+    id: 0,
+    code: "\u{1F9EC}",
+    name: "DNA",
+    grey: 104,
+  },
+  {
+    id: 1,
+    code: "\u{1F4D6}",
+    name: "Guides",
+    grey: 33,
+  },
+  {
+    id: 2,
+    code: "\u{1F3E5}",
+    name: "Health",
+    grey: 166,
+  },
+  {
+    id: 3,
+    code: "\u{1F389}",
+    name: "Lifestyle",
+    grey: 33,
+  },
+  {
+    id: 4,
+    code: "\u{1F9A0}",
+    name: "Microbiome",
+    grey: 79,
+  },
+  {
+    id: 5,
+    code: "\u{1F4F0}",
+    name: "News",
+    grey: 5,
+  },
+  {
+    id: 6,
+    code: "\u{1F951}",
+    name: "Nutrition",
+    grey: 166,
+  },
+  {
+    id: 7,
+    code: "\u{1F52C}",
+    name: "Oncology",
+    grey: 104,
+  },
+  {
+    id: 8,
+    code: "\u{1F35B}",
+    name: "Recipes",
+    grey: 5,
+  },
+  {
+    id: 9,
+    code: "\u{1F496}",
+    name: "Reviews",
+    grey: 244,
+  },
+];
