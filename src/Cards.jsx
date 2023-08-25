d2bbd4447389530fe3e265cd3545dbde9dcc88af
Ozod <bukhorizodao@gmail.com>
2023-08-25 12:20:19 +0500

    formated code

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -5,8 +5,8 @@ function Cards() {
     <div className="card" key={card.id}>
       <img src={card.img} alt={card.alt} className="card-img" />
       <div className="absolute-container">
-        <img src={card.absoluteImg} alt="" className="absolute-img"/>
-        <img src={card.playBtn} alt="" className="absolute-playBtn"/>
+        <img src={card.absoluteImg} alt="" className="absolute-img" />
+        <img src={card.playBtn} alt="" className="absolute-playBtn" />
       </div>
       <div className="card-details">
         <h3 className="card-h3">
@@ -24,23 +24,23 @@ function Cards() {
 function Articles() {
   return (
     <div className="article-details">
-      {articles.map((article) => 
+      {articles.map((article) => (
         <div className="detail" key={article.id}>
           <p className="number">{article.number}</p>
           <p className="advice">{article.name}</p>
         </div>
-      )}
+      ))}
     </div>
   );
 }
 export default function CardContainer() {
   return (
-      <div className="main-card-container">
-        <Cards />
-        <div className="articles-container">
+    <div className="main-card-container">
+      <Cards />
+      <div className="articles-container">
         <h3 className="article-para">Top articles</h3>
         <Articles />
       </div>
-      </div>
+    </div>
   );
 }
