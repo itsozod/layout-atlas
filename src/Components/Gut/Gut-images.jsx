e214f110a21e855258d72102074ced4318d08d14
Ozod <bukhorizodao@gmail.com>
2023-08-27 14:47:38 +0500

    changes in gut-images array file

--- src/Components/Gut/Gut-images.jsx
+++ src/Components/Gut/Gut-images.jsx
@@ -9,7 +9,7 @@ export const GutImages = [
     {
         id: 1, 
         img: 'microbal1.png',
-        
+        alt: 'Gut-Image',
         para: 'Ожирение ч. 2: риски, генитика, и лечение',
         grey: '9 апреля \u2022 8 мин',
     }
