195ae7030c41e44686e6288d29d3e0efe31b44b7
Ozod <bukhorizodao@gmail.com>
2023-08-31 19:34:56 +0500

    styled course component

--- src/Components/Course/Course-images.jsx
+++ src/Components/Course/Course-images.jsx
@@ -1,6 +1,26 @@
 export const courseImages = [
     {
         id: 0, 
-        
-    }
+        img: 'course 1.png',
+        alt: 'course',
+        info: 'Discover how stress impacts your gut health, including its impact on microbiome diversity and digestion.',
+    },
+    {
+        id: 1, 
+        img: 'course 2.png',
+        alt: 'course',
+        info: 'We are what we eat. Find out how to eat happily and healthy',
+    },
+    {
+        id: 2, 
+        img: 'course 3.png',
+        alt: 'course',
+        info: 'Put more colour in your diet to achieve a diverse microbal community in your gut',
+    },
+    {
+        id: 3, 
+        img: 'course 4.png',
+        alt: 'course',
+        info: 'Learn how to make your gut bacteria happier and how to take care of your microbiota',
+    },
 ];
--- src/Components/Course/Course.jsx
+++ src/Components/Course/Course.jsx
@@ -1,4 +1,54 @@
 import styles from "./Course.module.css";
-export default function Course() {
+import { courseImages } from "./Course-images";
 
+function CourseCards() {
+  return (
+    <div className={styles.course_container}>
+      <div className={styles.course_para_container}>
+        <h1 className={styles.big_para}>
+          <span className={styles.free_violet}>Free</span> Atlas Course
+        </h1>
+        <p className={styles.small_para}>
+          Collected the expertise of Atlas scientists and experts into short
+          email tutorials.
+        </p>
+        <p className={styles.small_para2}>
+          Subscribe to find out how digestion works and where to start making
+          dietary changes.
+        </p>
+      </div>
+      <div className={styles.absolute_book}>
+        <img
+          src="violet-book.png"
+          alt="Violet Book"
+          className={styles.violet_book}
+        />
+      </div>
+      <div className={styles.course_cards_container}>
+        {courseImages.map((courseImage) => (
+          <div className={styles.course_card} key={courseImage.id}>
+            <img
+              src={courseImage.img}
+              alt={courseImage.alt}
+              className={styles.course_card_img}
+            />
+            <div className={styles.course_card_details}>
+              <p className={styles.course_detail}>{courseImage.info}</p>
+            </div>
+          </div>
+        ))}
+        <div className={styles.absolute_button_container}>
+            <button className={styles.absolute_button}>All courses</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function Course() {
+  return (
+    <>
+      <CourseCards />
+    </>
+  );
 }
